2cba6480738faecdfb55806e7befaa937619e4ec
ixmjst <tjosesimao@gmail.com>
2026-05-18 09:32:06 +0100

    Update theme switch behavior and i18n asset files

--- frontend/src/app/shared/components/theme-switch/theme-switch.component.ts
+++ frontend/src/app/shared/components/theme-switch/theme-switch.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ThemeService } from '../../../core/services/theme.service';
 
 @Component({
@@ -7,9 +7,15 @@ import { ThemeService } from '../../../core/services/theme.service';
     styleUrls: ['./theme-switch.component.scss'],
 })
 export class ThemeSwitchComponent {
+    @Input() compact = false;
+
     currentTheme$ = this.themeService.currentTheme$;
 
     constructor(private themeService: ThemeService) { }
+
+    setTheme(theme: 'light' | 'dark'): void {
+        this.themeService.setTheme(theme);
+    }
 
     toggleTheme(): void {
         this.themeService.toggleTheme();
